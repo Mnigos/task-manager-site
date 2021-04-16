103b1da7e5a787b32cc313cfafc098fafcfbe25e
MoneyIgos <makowskiigor@gmail.com>
2021-04-16 16:26:07 +0200

    feat(providers): create TasksProvider

---
+++ src/providers/TasksProvider.tsx
@@ -0,0 +1,42 @@
+import { createContext, ReactNode, useState } from 'react'
+
+type Props = {
+  children?: ReactNode
+}
+
+type TasksContextType = {
+  tasks: string[]
+  // eslint-disable-next-line no-unused-vars
+  addTask(value: string): void
+  // eslint-disable-next-line no-unused-vars
+  deleteTask(taskToDelete: string): void
+}
+
+export const TasksContext = createContext<TasksContextType>({
+  tasks: [],
+  addTask: () => {},
+  deleteTask: () => {},
+})
+
+export default function TasksProvider({ children }: Props) {
+  const [tasks, setTasks] = useState<string[]>([])
+
+  const addTask = (value: string) => setTasks([value, ...tasks])
+
+  function deleteTask(taskToDelete: string) {
+    const filteredTasks = tasks.filter(task => task !== taskToDelete)
+    setTasks(filteredTasks)
+  }
+
+  return (
+    <TasksContext.Provider
+      value={{
+        tasks,
+        addTask,
+        deleteTask,
+      }}
+    >
+      {children}
+    </TasksContext.Provider>
+  )
+}
