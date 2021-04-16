75b05ab8da85581076b4240fb73067bfc638bfe3
MoneyIgos <makowskiigor@gmail.com>
2021-04-16 16:33:02 +0200

    refactor(TasksProvider): use PropsWithChildren instead of custom type

--- src/providers/TasksProvider.tsx
+++ src/providers/TasksProvider.tsx
@@ -1,8 +1,4 @@
-import { createContext, ReactNode, useState } from 'react'
-
-type Props = {
-  children?: ReactNode
-}
+import { createContext, PropsWithChildren, useState } from 'react'
 
 type TasksContextType = {
   tasks: string[]
@@ -18,7 +14,9 @@ export const TasksContext = createContext<TasksContextType>({
   deleteTask: () => {},
 })
 
-export default function TasksProvider({ children }: Props) {
+export default function TasksProvider({
+  children,
+}: PropsWithChildren<Record<never, any>>) {
   const [tasks, setTasks] = useState<string[]>([])
 
   const addTask = (value: string) => setTasks([value, ...tasks])
