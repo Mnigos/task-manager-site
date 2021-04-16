3777254b416317c4bfe1ac64793e3cadf1f6f639
MoneyIgos <makowskiigor@gmail.com>
2021-04-16 21:41:56 +0200

    test(TaskItem): add checking value displaying test

--- src/components/Tasks/TaskItem.spec.tsx
+++ src/components/Tasks/TaskItem.spec.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import { mount } from 'enzyme'
 
 import TaskItem from './TaskItem'
 
 describe('Task Item', () => {
   it('Renders component', () => {
     render(<TaskItem value="Do some work" />)
+  })
+
+  it('Value displays', () => {
+    const wrapper = mount(<TaskItem value="Do some work" />)
+
+    expect(wrapper.find('p').text()).toEqual('Do some work')
   })
 })
