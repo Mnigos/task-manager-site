447f486d8f1ae3003c80b550ec3e88fe93145a14
MoneyIgos <makowskiigor@gmail.com>
2021-04-18 18:37:35 +0200

    refactor(Tasks): checking if array is not empty before render

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -3,22 +3,30 @@ import React from 'react'
 import TaskItem from './TaskItem'
 import { StyledList, Wrapper } from './Tasks.styles'
 
+import { ITask } from 'interfaces/task.interface'
+
 interface TasksProps {
-  tasks: string[]
+  tasks: ITask[]
 }
 
 export default function Tasks({ tasks }: TasksProps) {
-  return (
-    <>
-      <Wrapper>
-        <StyledList>
-          {tasks.map((task, i) => (
-            <li key={i}>
-              <TaskItem value={task} />
-            </li>
-          ))}
-        </StyledList>
-      </Wrapper>
-    </>
+  return tasks.length ? (
+    <Wrapper>
+      <StyledList>
+        {tasks.map((task, i) => (
+          <li key={i}>
+            <TaskItem value={task.name} />
+          </li>
+        ))}
+      </StyledList>
+    </Wrapper>
+  ) : (
+    <Wrapper>
+      <p>No tasks found</p>
+    </Wrapper>
   )
+}
+
+Tasks.defaultProps = {
+  tasks: [],
 }
