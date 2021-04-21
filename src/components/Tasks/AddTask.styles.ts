1f0ecf0f2d415962ccc79fcba053feeecdc01ce7
MoneyIgos <makowskiigor@gmail.com>
2021-04-21 12:35:07 +0200

    feat(Tasks): create styles for AddTask component

---
+++ src/components/Tasks/AddTask.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const FormWrapper = styled.form`
+  margin: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
