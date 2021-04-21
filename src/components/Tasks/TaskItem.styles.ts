b44258da1934ea26672dfa040efbb32e13f876ce
MoneyIgos <makowskiigor@gmail.com>
2021-04-21 14:08:02 +0200

    refactor(TaskItem): change Item styling

--- src/components/Tasks/TaskItem.styles.ts
+++ src/components/Tasks/TaskItem.styles.ts
@@ -1,11 +1,23 @@
 import styled from 'styled-components'
 
+import { theme } from 'assets/styles/theme'
+
 export const Item = styled.p`
   margin: 10px;
   padding: 4px;
   max-width: 400px;
-  border: 2px solid black;
+  min-height: 50px;
+  display: flex;
+  font-size: ${theme.fontSize.xl};
+  align-items: center;
+  min-width: 200px;
   border-radius: 8px;
   display: flex;
   justify-content: center;
+  background-color: ${theme.colors.background};
+
+  &:hover {
+    cursor: pointer;
+    background: #1a1a1a;
+  }
 `
