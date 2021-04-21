c040842511346aea118a7af028cef2d6c713fa35
MoneyIgos <makowskiigor@gmail.com>
2021-04-21 12:02:55 +0200

    feat(items): add StyledLabel component

---
+++ src/components/items/Label.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+interface LabelProps {
+  size?: 's' | 'm' | 'l' | 'xl'
+}
+
+export const Label = styled.label<LabelProps>`
+  margin: 2px;
+
+  font-size: ${({ size }) => {
+    if (size === 'xl') return '42px'
+    else if (size === 'l') return '32px'
+    else if (size === 'm') return '24px'
+    else return '16px'
+  }};
+`
