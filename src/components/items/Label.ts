88ce5fd3153f131f02d8bee0a4173f1c8b45f372
MoneyIgos <makowskiigor@gmail.com>
2021-05-02 08:36:36 +0200

    refactor(Label): add color change option

--- src/components/items/Label.ts
+++ src/components/items/Label.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components'
 
+import { theme } from 'assets/styles/theme'
+
 interface LabelProps {
   size?: 's' | 'm' | 'l' | 'xl'
+  color?: 'error' | 'warn'
 }
 
 export const Label = styled.p<LabelProps>`
@@ -12,5 +15,11 @@ export const Label = styled.p<LabelProps>`
     else if (size === 'l') return '32px'
     else if (size === 'm') return '24px'
     else return '16px'
+  }};
+
+  color: ${({ color }) => {
+    if (color === 'error') return theme.colors.error
+    else if (color === 'warn') return theme.colors.warning
+    else return theme.colors.white
   }};
 `
