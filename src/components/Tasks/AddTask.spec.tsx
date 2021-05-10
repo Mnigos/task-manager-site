8d222bf79cb9ad71da5b68ad22501f89e7562cb4
MoneyIgos <makowskiigor@gmail.com>
2021-05-10 13:01:36 +0200

    test(AddTask): change test logic

--- src/components/Tasks/AddTask.spec.tsx
+++ src/components/Tasks/AddTask.spec.tsx
@@ -1,50 +1,48 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { render } from '@testing-library/react'
-import { mount } from 'enzyme'
+import { mount, ReactWrapper } from 'enzyme'
 
 import AddTask from './AddTask'
 
-const tasksMock = [
-  {
-    name: 'Task nr. One',
-  },
-  {
-    name: 'Task nr. Two',
-  },
-]
+import TasksProvider, { TasksContext } from 'providers/TasksProvider'
 
-const value = {
-  name: '',
+function TestComponent() {
+  const { tasks } = useContext(TasksContext)
+
+  return (
+    <>
+      <AddTask />
+      <p data-testid="tasks">
+        {tasks.map((task, i) => (
+          <i key={i}>{task.name}, </i>
+        ))}
+      </p>
+    </>
+  )
 }
 
 describe('Task Item', () => {
+  let wrapper: ReactWrapper
+
+  beforeEach(() => {
+    wrapper = mount(
+      <TasksProvider>
+        <TestComponent />
+      </TasksProvider>
+    )
+  })
+
   it('Renders component', () => {
     render(<AddTask />)
   })
 
   xit('Add task', () => {
-    const wrapper = mount(
-      <>
-        <AddTask />
-        <p>
-          {tasksMock.map((task, i) => (
-            <i key={i}>{task.name}, </i>
-          ))}
-        </p>
-        <b>{value.name}</b>
-      </>
-    )
-
-    const input = wrapper
+    wrapper
       .find('input')
       .simulate('change', { target: { value: 'Another task' } })
 
-    expect(input.props().value).toEqual('Another task')
-
     wrapper.find('button').simulate('click')
 
-    expect(wrapper.find('b').text()).toEqual(
-      'Task nr. One, Task nr. Two, Another Task'
-    )
+    expect(wrapper.find('[data-testid="tasks"]').text()).toEqual('Another Task')
   })
 })
