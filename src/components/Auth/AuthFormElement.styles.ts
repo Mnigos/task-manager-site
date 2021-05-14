7cc09d1ec8603a8d70463a0fdba205c95d739a98
MoneyIgos <makowskiigor@gmail.com>
2021-05-14 18:33:36 +0200

    refactor(auth): change FormWrapper margin 20px -> 15px

--- src/components/Auth/AuthFormElement.styles.ts
+++ src/components/Auth/AuthFormElement.styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const FormWrapper = styled.div`
-  margin: 20px;
+  margin: 15px;
   display: flex;
   flex-direction: column;
   justify-content: center;
