98aa494624d56086aaf277230f601861dccceac6
MoneyIgos <makowskiigor@gmail.com>
2021-07-27 15:30:51 +0200

    refactor(DeleteButton): change button styling

--- src/components/items/DeleteButton.tsx
+++ src/components/items/DeleteButton.tsx
@@ -3,21 +3,65 @@ import React, { MouseEventHandler } from 'react'
 
 import { theme } from 'assets/styles/theme'
 
-interface DeleteButtonProps {
+interface ButtonProps {
+  isDarker?: boolean
+  size?: 's' | 'm' | 'l' | 'xl'
+  type?: 'button' | 'submit' | 'reset'
+  shape?: 'raised' | 'ghost'
+  color?: 'primary' | 'white' | 'secondary' | 'black'
+}
+
+interface DeleteButtonProps extends ButtonProps {
   onClick: MouseEventHandler<HTMLButtonElement>
 }
 
-const StyledButton = styled.button`
+const StyledButton = styled.button<ButtonProps>`
+  background-color: ${({ shape, color }) => {
+    if (shape === 'ghost') return theme.colors.white
+    else return theme.colors[color || 'primary']
+  }};
+  color: ${({ color, shape }) => {
+    if (color === 'white') return theme.colors.black
+    if (shape === 'ghost') return theme.colors[color || 'primary']
+    else return theme.colors.white
+  }};
+  border: ${({ shape, color }) => {
+    if (shape === 'ghost')
+      return `1px ${theme.colors[color || 'primary']} solid`
+    else return 'none'
+  }};
+  width: ${({ size }) => {
+    if (size === 'xl') return '400px'
+    else if (size === 'l') return '320px'
+    else if (size === 'm') return '220px'
+    else return '70px'
+  }};
+  height: ${({ size }) => {
+    if (size === 'xl') return '150px'
+    else if (size === 'l') return '100px'
+    else if (size === 'm') return '80px'
+    else return '30px'
+  }};
+  box-shadow: ${({ shape }) => {
+    if (shape === 'ghost') return 'none'
+    else
+      return `0px 1px 2px rgba(0, 0, 0, 0.16), 0px 2px 4px rgba(0, 0, 0, 0.12),
+    0px 1px 8px rgba(0, 0, 0, 0.1)`
+  }};
   width: 30px;
   height: 30px;
-  background-color: #111;
-  border-radius: 50px;
+  background-color: ${theme.colors.primary};
+  border-radius: 4px;
   border: none;
   color: white;
   cursor: pointer;
 
-  &:hover {
-    background-color: ${theme.colors.black};
+  &:active {
+    transform: translatey(3px);
+
+    &::after {
+      transform: translatey(3px);
+    }
   }
 `
 
