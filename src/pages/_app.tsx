902eb780889e080cf0f31bc34813ba2c535208b2
MoneyIgos <makowskiigor@gmail.com>
2021-07-27 16:45:24 +0200

    feat(_app): add NavBar component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import { AppProps } from 'next/app'
 
-import { GlobalStyle } from '../assets/styles/GlobalStyle'
+import { GlobalStyle } from 'assets/styles/GlobalStyle'
+import NavBar from 'components/NavBar/NavBar'
 import 'assets/styles/fonts.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
+      <NavBar />
       <Component {...pageProps} />
     </>
   )
