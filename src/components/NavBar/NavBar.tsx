cbaffb5a4be45bed78aede11fe3008a6217dcba8
MoneyIgos <makowskiigor@gmail.com>
2021-07-28 14:36:27 +0200

    feat(NavBar): add onClick events to NavBar Auth buttons

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,3 +1,5 @@
+import router from 'next/router'
+
 import { Button } from '../items/Button'
 
 import { Wrapper, H1 } from './NavBar.styles'
@@ -9,8 +11,12 @@ export default function NavBar() {
         <H1>Task Manager</H1>
       </header>
       <div>
-        <Button color="white">Sign In</Button>
-        <Button color="secondary">Sign Up</Button>
+        <Button color="white" onClick={() => router.push('/auth/signin')}>
+          Sign In
+        </Button>
+        <Button color="secondary" onClick={() => router.push('/auth/signup')}>
+          Sign Up
+        </Button>
       </div>
     </Wrapper>
   )
