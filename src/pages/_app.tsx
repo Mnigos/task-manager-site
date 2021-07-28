d3c9b833452bcf82b87e424c2a544e69ddd6cb13
MoneyIgos <makowskiigor@gmail.com>
2021-07-28 14:53:46 +0200

    feat(pages): implement Footer to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,28 @@
 import { AppProps } from 'next/app'
+import styled from 'styled-components'
 
 import { GlobalStyle } from 'assets/styles/GlobalStyle'
 import NavBar from 'components/NavBar/NavBar'
+import Footer from 'components/Footer'
+
 import 'assets/styles/fonts.css'
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh;
+`
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Wrapper>
       <GlobalStyle />
-      <NavBar />
-      <Component {...pageProps} />
-    </>
+      <div>
+        <NavBar />
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </Wrapper>
   )
 }
