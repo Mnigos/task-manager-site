cdead516b59c1676f10856686bf3175ea757e321
MoneyIgos <makowskiigor@gmail.com>
2021-08-01 14:32:42 +0200

    feat(providers): create AuthProvider

---
+++ src/providers/AuthProvider.tsx
@@ -0,0 +1,42 @@
+import React, {
+  createContext,
+  FC,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
+import { auth } from 'firebase'
+import firebase from 'firebase/app'
+
+interface AuthContextType {
+  currentUser: firebase.User | null | undefined
+  signup(email: string, password: string): Promise<firebase.auth.UserCredential>
+}
+
+const AuthContext = createContext<AuthContextType>({
+  currentUser: null,
+  signup: () => {
+    throw new Error('Cannot find deleteTask function deffinition')
+  },
+})
+
+export const useAuth = () => useContext(AuthContext)
+
+export const AuthProvider: FC = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState<firebase.User | null>()
+
+  const signup = (email: string, password: string) =>
+    auth.createUserWithEmailAndPassword(email, password)
+
+  useEffect(() => {
+    return auth.onAuthStateChanged(user => {
+      setCurrentUser(user)
+    })
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ currentUser, signup }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
