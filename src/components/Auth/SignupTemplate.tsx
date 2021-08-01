59550d596e8893bdfbfd22449c5ad0c13b3f036e
MoneyIgos <makowskiigor@gmail.com>
2021-08-01 16:20:07 +0200

    feat(SignupTemplate): add handling Submit

--- src/components/Auth/SignupTemplate.tsx
+++ src/components/Auth/SignupTemplate.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { FormEvent, useRef } from 'react'
 import { useRouter } from 'next/router'
 
 import { Label } from '../items/Label'
@@ -7,8 +7,26 @@ import { Button } from '../items/Button'
 import AuthFormElement from './AuthFormElement'
 import { Wrapper, Header, Form, Alert } from './authTemplates.styles'
 
+import { useAuth } from 'providers/AuthProvider'
+
 export default function SignupTemplate() {
   const router = useRouter()
+  const { signup } = useAuth()
+
+  const credentials = {
+    username: useRef<HTMLInputElement>(null),
+    email: useRef<HTMLInputElement>(null),
+    password: useRef<HTMLInputElement>(null),
+    passwordConfirmation: useRef<HTMLInputElement>(null),
+  }
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const { email, password } = credentials
+
+    signup(email.current?.value as string, password.current?.value as string)
+  }
 
   return (
     <Wrapper>
@@ -18,11 +36,19 @@ export default function SignupTemplate() {
             Sign up to TaskManager
           </Label>
         </Header>
-        <Form>
-          <AuthFormElement name="Username" />
-          <AuthFormElement name="E-mail" />
-          <AuthFormElement name="Password" password />
-          <AuthFormElement name="Password Confirmation" password />
+        <Form onSubmit={handleSubmit}>
+          <AuthFormElement name="Username" ref={credentials.username} />
+          <AuthFormElement name="E-mail" ref={credentials.email} />
+          <AuthFormElement
+            name="Password"
+            password
+            ref={credentials.password}
+          />
+          <AuthFormElement
+            name="Password Confirmation"
+            password
+            ref={credentials.passwordConfirmation}
+          />
           <Button type="submit">Sign up</Button>
         </Form>
       </div>
