0c1dd850a204a66d84c6f20a91cfef01c5b15b8d
MoneyIgos <makowskiigor@gmail.com>
2021-08-10 09:32:24 +0200

    refactor(NavBar): add Header as router link to home

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,19 +2,23 @@ import router from 'next/router'
 
 import { Button } from '../items/Button'
 
-import { Wrapper, H1 } from './NavBar.styles'
+import { Wrapper, H1, HeaderButton } from './NavBar.styles'
 
 export default function NavBar() {
+  const push = (path: string) => () => router.push(path)
+
   return (
     <Wrapper>
       <header>
-        <H1>Task Manager</H1>
+        <HeaderButton onClick={push('/')}>
+          <H1>Task Manager</H1>
+        </HeaderButton>
       </header>
       <div>
-        <Button color="white" onClick={() => router.push('/auth/signin')}>
+        <Button color="white" onClick={push('/auth/signin')}>
           Sign In
         </Button>
-        <Button color="secondary" onClick={() => router.push('/auth/signup')}>
+        <Button color="secondary" onClick={push('/auth/signup')}>
           Sign Up
         </Button>
       </div>
